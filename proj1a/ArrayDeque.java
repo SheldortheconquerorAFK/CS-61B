347be67221ad252924d38857c164672724761dec
Steve Wozniak <Wozniak@microsoft.com>
2021-06-25 14:47:55 +0800

    Emm

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,9 +4,7 @@ public class ArrayDeque<T> {
     private T[] items;
     private int nextFirst;
     private int nextLast;
-    private int frontAdded;
-    private int lastAdded;
-    private int lastpointer;
+
 
 
     public ArrayDeque() {
@@ -14,33 +12,27 @@ public class ArrayDeque<T> {
         items = (T[]) new Object[8];
         nextFirst = 0;
         nextLast = 1;
-        frontAdded = 0;
-        lastAdded = 0;
 
+    }
+
+    public int addOne(int index) {
+        return (index + 1) % items.length;
+    }
+
+    public int minusOne(int index) {
+        return (index - 1 + items.length) % items.length;
     }
 
     public void addFirst(T item) {
-        if (nextFirst == 0) {
-            items[nextFirst] = item;
-            nextFirst = items.length - 1;
-        } else {
-            items[nextFirst] = item;
-            nextFirst -= 1;
-        }
+        items[nextFirst] = item;
+        nextFirst = addOne(nextFirst);
         size += 1;
-        frontAdded += 1;
     }
 
     public void addLast(T item) {
-        if (nextLast == items.length - 1) {
-            items[nextLast] = item;
-            nextLast = 0;
-        } else {
-            items[nextLast] = item;
-            nextLast += 1;
-        }
+        items[nextLast] = item;
+        nextLast = minusOne(nextLast);
         size += 1;
-        lastAdded += 1;
     }
 
     public boolean isEmpty() {
@@ -52,13 +44,11 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
-        for (int i = nextFirst + 1; i < nextLast; i++) {
-            if (i == items.length - 1) {
-                System.out.println(items[i] + " ");
-                i = -1;
-                continue;
-            }
+        int i = addOne(nextFirst);
+        for (int j = 0; j < size(); j++) {
+
             System.out.println(items[i] + " ");
+            i = addOne(nextFirst);
         }
         System.out.println("\n");
     }
@@ -67,87 +57,29 @@ public class ArrayDeque<T> {
         if (size() == 0) {
             return null;
         }
-        T value = items[frontInd()];    // take the item of zeroth index
-        items[frontInd()] = null;
-        if (frontAdded == 0) {
-            lastAdded -= 1;
-
-        } else {
-            frontAdded -= 1;
-            if (nextFirst == items.length) {
-                nextFirst = 0;
-            } else {
-                nextFirst = nextFirst + 1;
-            }
-        }
+        T value = items[addOne(nextFirst)];
+        items[addOne(nextFirst)] = null;
+        nextFirst = addOne(nextFirst);
         size -= 1;
         return value;
-    }
 
-    private int frontInd() {
-        if (frontAdded > 0) {
-            if (nextFirst == items.length) {
-                return 0;
-            } else {
-                return nextFirst + 1;
-            }
-        } else {
-            if (nextLast <= 1) {
-                return items.length - (size() - nextLast);
-            } else {
-                return nextLast - size();
-            }
-        }
+
     }
 
     public T removeLast() {
         if (size() == 0) {
             return null;
         }
-        T value = items[lastInd()];     // take the item of the last index
-        items[lastInd()] = null;
-        if (lastAdded == 0) {
-            frontAdded -= 1;
-
-        } else{
-            lastAdded -= 1;
-            if (nextLast == 0){
-                nextLast = items.length - 1;
-            } else {
-                nextLast = nextLast - 1;
-            }
-        }
+        T value = items[minusOne(nextLast)];
+        items[addOne(nextFirst)] = null;
+        nextLast = minusOne(nextLast);
         size -= 1;
         return value;
     }
 
-    private int lastInd() {
-        if (lastAdded > 0) {
-            if (nextLast == 0) {
-                return items.length - 1;
-            } else {
-                return nextLast - 1;
-            }
-        } else {
-            if (size() == 1 || size() == items.length) {
-                return 0;
-            } else {
-                return frontAdded;
-            }
-        }
-    }
-
-    /**
-     * Take the zeroth item from the list(using frontInd function), then add index onto it.
-     * @param index the index of the list(not the underlying array)
-     * @return value of the item on slot of the given index
-     */
     public T get(int index) {
-        if (frontInd() + index >= items.length) {
-            return items[frontInd() + index - items.length];
-        } else {
-            return items[frontInd() + index];
-        }
+        int start = addOne(nextFirst);
+        return items[(start + index) % items.length];
     }
 
 }
