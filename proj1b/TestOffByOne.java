515247ce92aa67ab7c67d5a7721c9d2d106674a6
Steve Wozniak <Wozniak@microsoft.com>
2021-06-30 11:50:24 +0800

    Project 1B

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -16,6 +16,6 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
         assertTrue(offByOne.equalChars('&', '%'));
-        assertFalse(offByOne.equalChars('a', 'A'));
+        assertTrue(offByOne.equalChars('a', 'A'));
     }
 }
