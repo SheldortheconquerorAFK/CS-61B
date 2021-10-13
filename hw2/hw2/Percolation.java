104e19dbaf1046acc250393fdd11d87d80692da4
Kevin James <useforcracking@gmail.com>
2021-10-13 18:21:19 +0800

    HW2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -5,17 +5,54 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     WeightedQuickUnionUF grid;
     int length;
+    boolean[] isSiteOpen;
+    int sitesOpened;
+    int virtualTopSiteIndex;
+    int virtualBottomSiteIndex;
 
     public Percolation(int N) {
         if (N <= 0) {
             throw new IllegalArgumentException("N must be an integer bigger than 0.");
         }
-        WeightedQuickUnionUF grid = new WeightedQuickUnionUF((int) Math.pow(N, 2));
+        WeightedQuickUnionUF grid = new WeightedQuickUnionUF((int) Math.pow(N, 2) + 2);
         length = N;
+        virtualTopSiteIndex = (int) Math.pow(N, 2);
+        virtualBottomSiteIndex = (int) Math.pow(N, 2) + 1;
+        sitesOpened = 0;
+
+        isSiteOpen = new boolean[(int) Math.pow(N, 2) + 1];
+        for (int i = 0; i < (int) Math.pow(N, 2) - 1; i++) {
+            isSiteOpen[i] = false;
+        }
+        isSiteOpen[virtualTopSiteIndex] = true;
+        isSiteOpen[virtualBottomSiteIndex] = true;
     }
 
     public void open(int row, int col) {
+        if (row == 0 && !isSiteOpen[calcArrayIndex(row, col)]) {
+            grid.union(calcArrayIndex(row, col), virtualTopSiteIndex);  // Connect top row site with the virtual top site
+        }
+        if (row == length - 1 && !isSiteOpen[calcArrayIndex(row, col)]) {
+            grid.union(calcArrayIndex(row, col), virtualBottomSiteIndex); // Connect bottom row site with the virtual bottom site
+        }
+        isSiteOpen[calcArrayIndex(row, col)] = true;  // Mark specified site as "open"
+        sitesOpened++;  // Add number of sites opened by 1
+        connectNeighbour(row, col); // Check if site on four directions are open. If yes, union them
+    }
 
+    private void connectNeighbour(int row, int col) {
+        if (row != 0 && isSiteOpen[calcArrayIndex(row - 1, col)]) {
+            grid.union(calcArrayIndex(row, col), calcArrayIndex(row - 1, col));
+        }
+        if (row != length - 1 && isSiteOpen[calcArrayIndex(row + 1, col)]) {
+            grid.union(calcArrayIndex(row, col), calcArrayIndex(row + 1, col));
+        }
+        if (col != 0 && isSiteOpen[calcArrayIndex(row, col - 1)]) {
+            grid.union(calcArrayIndex(row, col), calcArrayIndex(row, col - 1));
+        }
+        if (col != length - 1 && isSiteOpen[calcArrayIndex(row, col + 1)]) {
+            grid.union(calcArrayIndex(row, col), calcArrayIndex(row, col + 1));
+        }
     }
 
     private int calcArrayIndex(int row, int col) {
@@ -23,5 +60,26 @@ public class Percolation {
             throw new IndexOutOfBoundsException("The row or column number is out of length of the grid.");
         }
         return length * row + col;
+    }
+
+    public boolean isOpen(int row, int col) {
+        return isSiteOpen[calcArrayIndex(row, col)];
+    }
+
+    public boolean isFull(int row, int col) {
+        return grid.find(calcArrayIndex(row, col)) == grid.find(virtualTopSiteIndex);
+    }
+
+    public int numberOfOpenSites() {
+        return sitesOpened;
+    }
+
+    public boolean percolates() {
+        for (int i = 0; i < length; i++) {
+            if (isFull(length - 1, i)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
