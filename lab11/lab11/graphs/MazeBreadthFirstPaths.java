69b6d8b1ce604358b1bd3124d8cc95388253ac9c
Kevin James <useforcracking@gmail.com>
2021-11-14 13:41:25 +0800

    lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -18,7 +18,6 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
         maze = m;
         s = maze.xyTo1D(sourceX, sourceY);
         t = maze.xyTo1D(targetX, targetY);
@@ -32,7 +31,6 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
         dq.remove(s);
         for (int w : maze.adj(s)) {
             if (!marked[w]) {
@@ -60,22 +58,8 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
                     announce();
                 }
             }
-
         }
     }
-
-    private void bfs(int v) {
-        marked[v] = true;
-        dq.remove(v);
-        for (int w : maze.adj(v)) {
-            if (!marked[w]) {
-                edgeTo[w] = v;
-                distTo[w] = distTo[v] + 1;
-
-            }
-        }
-    }
-
 
     @Override
     public void solve() {
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,6 +11,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean hasCircle = false;
+    private boolean[] onStack = new boolean[maze.V()];
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,9 +20,27 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        edgeTo[0] = 0;
+        findCycle(0);
     }
 
     // Helper methods go here
+    private void findCycle(int v) {
+        marked[v] = true;
+        onStack[v] = true;
+        announce();
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                findCycle(w);
+            }
+            if (onStack[w] && edgeTo[v] != w) {
+                hasCircle = true;
+                return;
+            }
+        }
+        onStack[v] = false;
+        announce();
+    }
 }
-
