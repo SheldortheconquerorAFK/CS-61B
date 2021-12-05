05c0aace24cfd39fe17262c01b8ab01125b00463
Kevin James <useforcracking@gmail.com>
2021-12-05 10:37:40 +0800

    lab12

--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -47,8 +47,7 @@ public class QuickSort {
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
-        while (!unsorted.isEmpty()) {
-            Item item = unsorted.dequeue();
+        for (Item item : unsorted) {
             if (item.compareTo(pivot) < 0) {
                 less.enqueue(item);
             } else if (item.compareTo(pivot) == 0) {
