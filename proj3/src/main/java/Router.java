c443b60ba1155f53fb995c95417c7b27e94d18bb
Kevin James <useforcracking@gmail.com>
2021-12-15 01:37:06 -0800

    Project 3, Part I

--- proj3/src/main/java/Router.java
+++ proj3/src/main/java/Router.java
@@ -33,7 +33,7 @@ public class Router {
      * @param g The graph to use.
      * @param route The route to translate into directions. Each element
      *              corresponds to a node from the graph in the route.
-     * @return A list of NavigatiionDirection objects corresponding to the input
+     * @return A list of NavigationDirection objects corresponding to the input
      * route.
      */
     public static List<NavigationDirection> routeDirections(GraphDB g, List<Long> route) {
