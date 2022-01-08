c3f9db012f2cb9df55e73eb6873f20204445cf7c
Kevin James <useforcracking@gmail.com>
2022-01-07 20:20:40 -0800

    HW5

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -6,13 +6,44 @@ import java.awt.Color;
 public class SeamCarver {
     Picture p;
     double[][] energyMatrix;
+    boolean isOriginReset;
 
     public SeamCarver(Picture picture) {
         p = picture;
         energyMatrix = new double[height()][width()];
+        isOriginReset = false;
         for (int y = 0; y < height(); y++) {
             for (int x = 0; x < width(); x++) {
-                energyMatrix[y][x] = energy(x, y);
+                int xMinusOne = x - 1;
+                int xPlusOne = x + 1;
+                int yMinusOne = y - 1;
+                int yPlusOne = y + 1;
+                if (x == 0) {
+                    xMinusOne = width() - 1;
+                }
+                if (x == width() - 1) {
+                    xPlusOne = 0;
+                }
+                if (y == 0) {
+                    yMinusOne = height() - 1;
+                }
+                if (y == height() - 1) {
+                    yPlusOne = 0;
+                }
+                Color left = p.get(xMinusOne, y);
+                Color right = p.get(xPlusOne, y);
+                Color up = p.get(x, yMinusOne);
+                Color down = p.get(x, yPlusOne);
+
+                double xGradient = (right.getRed() - left.getRed()) * (right.getRed() - left.getRed())
+                        + (right.getGreen() - left.getGreen()) * (right.getGreen() - left.getGreen())
+                        + (right.getBlue() - left.getBlue()) * (right.getBlue() - left.getBlue());
+
+                double yGradient = (down.getRed() - up.getRed()) * (down.getRed() - up.getRed())
+                        + (down.getGreen() - up.getGreen()) * (down.getGreen() - up.getGreen())
+                        + (down.getBlue() - up.getBlue()) * (down.getBlue() - up.getBlue());
+
+                energyMatrix[y][x] = xGradient + yGradient;
             }
         }
     }
@@ -22,114 +53,90 @@ public class SeamCarver {
     }
 
     public int width() {
-        return p.width();
+        if (isOriginReset) {
+            return p.height();
+        } else {
+            return p.width();
+        }
     }
 
     public int height() {
-        return p.height();
+        if (isOriginReset) {
+            return p.width();
+        } else {
+            return p.height();
+        }
     }
 
     public double energy(int x, int y) {
-        validateColumnIndex(x);
-        validateRowIndex(y);
-
-        int xMinusOne = x - 1;
-        int xPlusOne = x + 1;
-        int yMinusOne = y - 1;
-        int yPlusOne = y + 1;
-        if (x == 0) {
-            xMinusOne = width() - 1;
+        if (isOriginReset) {
+            int oldY = p.height() - 1 - x;
+            int oldX = y;
+            return energyMatrix[oldY][oldX];
+        } else {
+            return energyMatrix[y][x];
         }
-        if (x == width() - 1) {
-            xPlusOne = 0;
-        }
-        if (y == 0) {
-            yMinusOne = height() - 1;
-        }
-        if (y == height() - 1) {
-            yPlusOne = 0;
-        }
-        Color left = p.get(xMinusOne, y);
-        Color right = p.get(xPlusOne, y);
-        Color up = p.get(x, yMinusOne);
-        Color down = p.get(x, yPlusOne);
-
-        double xGradient = (right.getRed() - left.getRed()) * (right.getRed() - left.getRed())
-                + (right.getGreen() - left.getGreen()) * (right.getGreen() - left.getGreen())
-                + (right.getBlue() - left.getBlue()) * (right.getBlue() - left.getBlue());
-
-        double yGradient = (down.getRed() - up.getRed()) * (down.getRed() - up.getRed())
-                + (down.getGreen() - up.getGreen()) * (down.getGreen() - up.getGreen())
-                + (down.getBlue() - up.getBlue()) * (down.getBlue() - up.getBlue());
-
-        return xGradient + yGradient;
     }
-
-
 
     public int[] findHorizontalSeam() {
         return null;
     }
 
     public int[] findVerticalSeam() {
-        double[] seamEnergy = new double[width()];
-        for (int x = 0; x < width(); x++) {
-            int[] seamForX = findVerticalPath(x, 0);
-            double[] energy = new double[height()];
-            for (int y = 0; y < height(); y++) {
-                energy[y] = energyMatrix[y][seamForX[y]];
-            }
-            double totalEnergy = 0;
-        }
-        return new int[1];
-    }
-
-    private int[] findVerticalPath(int x, int y) {
-        validateColumnIndex(x);
-        validateRowIndex(y);
-
-        int[] path = new int[height() - y];
-        path[0] = x;
-        int lastX = x;
-        for (int row = 1; row < height() - y; row++) {
-            if (lastX == 0) {
-                if (energyMatrix[y + row][x] <= energyMatrix[y + row][x + 1]) {
-                    path[row] = x;
-                    lastX = x;
+        double[][] pathEnergyRecord = new double[height()][width()];
+        int[][] nextRowPixelFrom = new int[height()][width()];
+        for (int row = 0; row < height(); row++) {
+            for (int col = 0; col < width(); col++) {
+                if (row == 0) {
+                    pathEnergyRecord[row][col] = energy(col, row);
+                    nextRowPixelFrom[row][col] = col;
                 } else {
-                    path[row] = x + 1;
-                    lastX = x + 1;
-                }
-            } else if (lastX == width() - 1) {
-                if (energyMatrix[y + row][x] <= energyMatrix[y + row][x - 1]) {
-                    path[row] = x;
-                    lastX = x;
-                } else {
-                    path[row] = x - 1;
-                    lastX = x - 1;
-                }
-            } else {
-                if (StdStats.min(new double[]{energyMatrix[y + row][x - 1], energyMatrix[y + row][x], energyMatrix[y + row][x + 1]}) == energyMatrix[y + row][x - 1]) {
-                    path[row] = x - 1;
-                    lastX = x - 1;
-                } else if (StdStats.min(new double[]{energy(x - 1, y + row), energy(x, y + row), energy(x + 1, y + row)}) == energyMatrix[y + row][x]) {
-                    path[row] = x;
-                    lastX = x;
-                } else {
-                    path[row] = x + 1;
-                    lastX = x + 1;
+                    if (col == 0) {
+                        pathEnergyRecord[row][col] = energy(col, row) + StdStats.min(new double[]{pathEnergyRecord[row - 1][col], pathEnergyRecord[row - 1][col + 1]});
+                        if (StdStats.min(new double[]{pathEnergyRecord[row - 1][col], pathEnergyRecord[row - 1][col + 1]}) == pathEnergyRecord[row - 1][col]) {
+                            nextRowPixelFrom[row][col] = col;
+                        } else {
+                            nextRowPixelFrom[row][col] = col + 1;
+                        }
+                    } else if (col == width() - 1) {
+                        pathEnergyRecord[row][col] = energy(col, row) + StdStats.min(new double[]{pathEnergyRecord[row - 1][col - 1], pathEnergyRecord[row - 1][col]});
+                        if (StdStats.min(new double[]{pathEnergyRecord[row - 1][col - 1], pathEnergyRecord[row - 1][col]}) == pathEnergyRecord[row - 1][col]) {
+                            nextRowPixelFrom[row][col] = col - 1;
+                        } else {
+                            nextRowPixelFrom[row][col] = col;
+                        }
+                    } else {
+                        pathEnergyRecord[row][col] = energy(col, row) + StdStats.min(new double[]{pathEnergyRecord[row - 1][col - 1], pathEnergyRecord[row - 1][col], pathEnergyRecord[row - 1][col + 1]});
+                        if (StdStats.min(new double[]{pathEnergyRecord[row - 1][col], pathEnergyRecord[row - 1][col + 1]}) == pathEnergyRecord[row - 1][col]) {
+                            nextRowPixelFrom[row][col] = col - 1;
+                        } else if (StdStats.min(new double[]{pathEnergyRecord[row - 1][col], pathEnergyRecord[row - 1][col + 1]}) == pathEnergyRecord[row][col]){
+                            nextRowPixelFrom[row][col] = col;
+                        } else {
+                            nextRowPixelFrom[row][col] = col + 1;
+                        }
+                    }
                 }
             }
         }
-        return path;
+        double minPathEnergy = StdStats.min(pathEnergyRecord[height() - 1]);
+        int[] seam = new int[height()];
+        for (int i = 0; i < width(); i++) {
+            if (pathEnergyRecord[height() - 1][i] == minPathEnergy) {
+                seam[height() - 1] = i;
+                for (int j = height() - 1; j > 0; j--) {
+                    seam[j - 1] = nextRowPixelFrom[j][seam[j]];
+                }
+            }
+        }
+        return seam;
     }
 
     public void removeHorizontalSeam(int[] seam) {
-
+        SeamRemover.removeHorizontalSeam(p, seam);
     }
 
     public void removeVerticalSeam(int[] seam) {
-
+        SeamRemover.removeVerticalSeam(p, seam);
     }
 
     private void validateRowIndex(int y) {
